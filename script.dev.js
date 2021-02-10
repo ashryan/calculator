071a6ce15d60b97473bf0e57e7e6daed977d17dc
Ashlee <Ashlee.ryan@outlook.com>
2021-02-10 21:46:16 +0000

    added calculator logic

--- script.dev.js
+++ script.dev.js
@@ -1,1 +1,68 @@
 "use strict";
+
+var getHistory = function getHistory() {
+  return document.querySelector("#history-value").innerHTML;
+};
+
+var output = document.querySelector("#output-value").innerHTML;
+
+var setHistory = function setHistory(num) {
+  document.querySelector("#history-value").innerHTML = num;
+};
+
+var getOutput = function getOutput() {
+  return document.querySelector("#output-value").innerHTML;
+};
+
+var setOutput = function setOutput(num) {
+  document.querySelector("#output-value").innerHTML = num;
+};
+
+var funcCol = document.getElementsByClassName("func");
+var numCol = document.getElementsByClassName("num-char"); // iterates through function chars and adds event listner
+
+var _loop = function _loop(i) {
+  funcCol[i].addEventListener('click', function (event) {
+    if (funcCol[i].innerHTML == "AC") {
+      setOutput("0");
+      setHistory("0");
+    } else if (funcCol[i].innerHTML == "+") {
+      setHistory(getOutput() + "+");
+      setOutput("0");
+    } else if (funcCol[i].innerHTML == "-") {
+      setHistory(getOutput() + "-");
+      setOutput("0");
+    } else if (funcCol[i].innerHTML == "=") {
+      var combined = getHistory() + getOutput();
+      var result = eval(combined);
+      var history = document.querySelector("#history-value").innerHTML;
+      setHistory(getHistory() + getOutput());
+      setOutput(result);
+    } else if (funcCol[i].innerHTML == "x") {
+      setHistory(getOutput() + "*");
+      setOutput("0");
+    } else if (funcCol[i].innerHTML == "÷") {
+      setHistory(getOutput() + "/");
+      setOutput("0");
+    }
+  });
+};
+
+for (var i = 0; i < funcCol.length; i++) {
+  _loop(i);
+} //iterates through num chars and adds event listner, changes output text to key pressed
+
+
+for (var _i = 0; _i < numCol.length; _i++) {
+  numCol[_i].addEventListener('click', function (event) {
+    // if output is 0 then replace output with num pressed
+    if (document.querySelector("#output-value").innerHTML == 0) {
+      var numClicked = event.target.innerHTML;
+      setOutput(numClicked); // if num is not 0 concat num pressed to current output
+    } else {
+      var _numClicked = event.target.innerHTML;
+      output = getOutput();
+      setOutput(output + _numClicked);
+    }
+  });
+}
---
+++ script.js
@@ -0,0 +1,84 @@
+let getHistory = () => {
+   return document.querySelector("#history-value").innerHTML;
+}
+
+let output = document.querySelector("#output-value").innerHTML
+
+let setHistory = (num) => {
+    document.querySelector("#history-value").innerHTML=num;
+}
+
+let getOutput= () => {
+    return document.querySelector("#output-value").innerHTML;
+}
+
+let setOutput = (num) => {
+
+    document.querySelector("#output-value").innerHTML=num;
+}
+
+let funcCol = document.getElementsByClassName("func");
+
+
+let numCol = document.getElementsByClassName("num-char");
+
+
+// iterates through function chars and adds event listner
+for(let i = 0; i < funcCol.length; i++) {
+    funcCol[i].addEventListener('click', (event)=>{
+        if(funcCol[i].innerHTML == "AC") {
+            setOutput("0");
+            setHistory("0");
+        }  else if(funcCol[i].innerHTML == "+") {
+            setHistory(getOutput() + "+") 
+            setOutput("0")
+        } 
+        else if(funcCol[i].innerHTML == "-") {
+            setHistory(getOutput() + "-")
+            setOutput("0")
+        }
+
+        else if(funcCol[i].innerHTML == "=") {
+            let combined = getHistory() + getOutput()
+             let result = eval(combined)
+             let history = document.querySelector("#history-value").innerHTML
+
+             setHistory(getHistory() + getOutput())
+             setOutput(result)
+        }
+
+        else if(funcCol[i].innerHTML == "x") {
+            setHistory(getOutput() + "*");
+            setOutput("0");
+        }
+
+        else if(funcCol[i].innerHTML == "÷") {
+            setHistory(getOutput() + "/")
+            setOutput("0")
+        }
+
+
+    })
+}
+
+
+
+//iterates through num chars and adds event listner, changes output text to key pressed
+for(let i = 0; i < numCol.length; i++) {
+    numCol[i].addEventListener('click', (event)=>{
+        // if output is 0 then replace output with num pressed
+      if(document.querySelector("#output-value").innerHTML == 0) { let numClicked = event.target.innerHTML;
+        setOutput(numClicked);
+        // if num is not 0 concat num pressed to current output
+        } else { 
+            let numClicked = event.target.innerHTML
+            output = getOutput()
+            setOutput(output + numClicked)
+        }
+    } 
+  )
+}
+
+
+
+
